ee479e4e94c6eb89f317099eaf693644a03d1915
Ryuta Hamasaki <hamasaki@kurashicom.jp>
2017-10-01 14:17:26 +0900

    switch sidenav mode depending on window width

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { MdSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  @ViewChild('sidenav')
+  sidenav: MdSidenav;
+
+  navMode = 'side';
+
+  navOpened = true;
+
+  ngOnInit(): void {
+    if (window.innerWidth < 768) {
+      this.navMode   = 'over';
+      this.navOpened = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (event.target.innerWidth < 768) {
+        this.navMode   = 'over';
+        this.navOpened = false;
+    } else {
+      this.navMode   = 'side';
+      this.navOpened = true;
+    }
+  }
 }
